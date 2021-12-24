5855f3293ae964af58acf78e805e352c95b90c83
Bing35 <69435914+Bing35@users.noreply.github.com>
2021-12-24 14:49:47 -0500

    added minify and unminify script for .js files

---
+++ src/minify.mjs
@@ -0,0 +1,47 @@
+import { minify } from "terser"
+import * as fs from "fs/promises"
+import * as path from "path"
+
+const publicPath = path.join(`${path.resolve()}/public`)
+
+//* script to minify
+const scriptPath = path.join(`${path.resolve()}/public/script`)
+const files = await fs.readdir(scriptPath, {withFileTypes: true})
+for(const file of files){
+    if(file.isFile() || !file.name.match(/\.html$/)){
+        const filePath = path.join(`${scriptPath}/${file.name}`)
+        const codeStr = await fs.readFile(filePath, {encoding: 'utf-8'})
+        const minifiedStr = (await minify(codeStr)).code
+        const minifiedPath = path.join(`${path.resolve()}/public/min/${file.name}`)
+        await fs.writeFile(minifiedPath, minifiedStr)
+    }
+}
+
+
+
+// * modify html links
+const htmlPath = path.join(`${path.resolve()}/public`)
+const fileList = await fs.readdir(htmlPath, {withFileTypes: true})
+
+// let testCounter = 0
+for(const file of fileList){
+    if(!file.isFile()){
+        continue
+    }
+
+    // // running once for testing
+    // if(testCounter > 0){
+    //     break
+    // }
+    // console.log('min html', file.name);
+    // testCounter+=1
+
+    const filePath = path.join(`${htmlPath}/${file.name}`)
+    const htmlStr = await fs.readFile(filePath, {encoding: 'utf-8'})
+    const minHtml = htmlStr.replace(/script\//g, 'min/')
+    await fs.writeFile(filePath, minHtml)
+}
+
+
+
+
